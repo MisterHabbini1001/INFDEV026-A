e8407f9afe0468b65aca26c46a1209a092b45aaa
MisterHabbini1001 <0910681@hr.nl>
2016-11-16 14:00:30 +0100

    Backup function stub implementation + Pseudocode
    Added 2 new files to the EntryPoint project: Function stub
    implementation and pseudocode. In function stub implementation, I will
    make a backup of the stub implementation of the functions in program.cs.
    That way, if something might go wrong while writing my own
    implementations, I can just copy and paste.
    
    In pseudocode (like the title suggests), I will place the pseudocode for
    each exercise/assignment. That way, I will have written down in "normal"
    human language what the function should implement before I will
    translate it to C#.

---
+++ Assignment/EntryPoint/Functions stub implementations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryPoint
+{
+    class Functions_stub_implementations
+    {
+        /*
+         private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings) // EXERCISE 1 - Sorting  EXERCISE 1 - Sorting  EXERCISE 1 - Sorting  !!!!!!
+        {
+      return specialBuildings.OrderBy(v => Vector2.Distance(v, house));
+    }
+
+    private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse( // EXERCISE 2 - Trees  EXERCISE 2 - Trees  EXERCISE 2 - Trees  EXERCISE 2 - Trees !!!!!!!!
+      IEnumerable<Vector2> specialBuildings, 
+      IEnumerable<Tuple<Vector2, float>> housesAndDistances)
+    {
+      return
+          from h in housesAndDistances
+          select
+            from s in specialBuildings
+            where Vector2.Distance(h.Item1, s) <= h.Item2
+            select s;
+    }
+
+    private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding, // EXERCISE 3 - Graphs  Option 1: Dijkstra  EXERCISE 3 - Graphs  Option 1: Dijkstra  EXERCISE 3 - Graphs  Option 1: Dijkstra !!!!!!!!!
+      Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+      List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+      var prevRoad = startingRoad;
+      for (int i = 0; i < 30; i++)
+      {
+        prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, destinationBuilding)).First());
+        fakeBestPath.Add(prevRoad);
+      }
+      return fakeBestPath;
+    }
+
+    private static IEnumerable<IEnumerable<Tuple<Vector2, Vector2>>> FindRoutesToAll(Vector2 startingBuilding, // EXERCISE 3 - Graphs  Option 2: Floyd-Marshall  EXERCISE 3 - Graphs  Option 2: Floyd-Marshall  EXERCISE 3 - Graphs  Option 2: Floyd-Marshall !!!!!!!!!
+      IEnumerable<Vector2> destinationBuildings, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      List<List<Tuple<Vector2, Vector2>>> result = new List<List<Tuple<Vector2, Vector2>>>();
+      foreach (var d in destinationBuildings)
+      {
+        var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+        List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+        var prevRoad = startingRoad;
+        for (int i = 0; i < 30; i++)
+        {
+          prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, d)).First());
+          fakeBestPath.Add(prevRoad);
+        }
+        result.Add(fakeBestPath);
+      }
+      return result;
+    }
+        */
+    }
+}
---
+++ Assignment/EntryPoint/Pseudocode exercises.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryPoint
+{
+    class Pseudocode_exercises
+    {
+        /*
+         private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings) // EXERCISE 1 - Sorting  EXERCISE 1 - Sorting  EXERCISE 1 - Sorting  !!!!!!
+        {
+      
+    }
+
+    private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse( // EXERCISE 2 - Trees  EXERCISE 2 - Trees  EXERCISE 2 - Trees  EXERCISE 2 - Trees !!!!!!!!
+      IEnumerable<Vector2> specialBuildings, 
+      IEnumerable<Tuple<Vector2, float>> housesAndDistances)
+    {
+      
+    }
+
+    private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding, // EXERCISE 3 - Graphs  Option 1: Dijkstra  EXERCISE 3 - Graphs  Option 1: Dijkstra  EXERCISE 3 - Graphs  Option 1: Dijkstra !!!!!!!!!
+      Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      
+    }
+
+    private static IEnumerable<IEnumerable<Tuple<Vector2, Vector2>>> FindRoutesToAll(Vector2 startingBuilding, // EXERCISE 3 - Graphs  Option 2: Floyd-Marshall  EXERCISE 3 - Graphs  Option 2: Floyd-Marshall  EXERCISE 3 - Graphs  Option 2: Floyd-Marshall !!!!!!!!!
+      IEnumerable<Vector2> destinationBuildings, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      
+    }
+        */
+    }
+}
+
+/*
+EXERCISE 1 - Sorting:
+
+EXERCISE 2 - Trees:
+
+EXERCISE 3 - Graphs  Option 1: Dijkstra:
+
+EXERCISE 3 - Graphs  Option 2: Floyd-Marshall:
+*/
