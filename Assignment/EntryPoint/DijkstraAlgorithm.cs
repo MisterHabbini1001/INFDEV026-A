815b270f55d185eabf27f45ae3f4754efa588dfd
MisterHabbini1001 <0910681@hr.nl>
2016-12-09 15:33:49 +0100

    Dijkstra comments
    Added some comments to the Dijkstra exercise

--- Assignment/EntryPoint/DijkstraAlgorithm.cs
+++ Assignment/EntryPoint/DijkstraAlgorithm.cs
@@ -12,10 +12,11 @@ namespace EntryPoint
         public static IEnumerable<Tuple<Vector2, Vector2>> RoadDetermination(Vector2 startingBuilding, Vector2 destinationBuilding, List<Tuple<Vector2, Vector2>> roads) 
         {
             Graph graph = new Graph();                                                // Graph is empty
-            graph = insertGraph(graph, roads, startingBuilding, destinationBuilding); // Graph gets created here
+            graph = InsertGraph(graph, roads, startingBuilding, destinationBuilding); // Graph gets created here
 
             List<Tuple<Vector2, Vector2>> resultListBA = new List<Tuple<Vector2, Vector2>>();
-            resultListBA = graph.ShortestPath(startingBuilding, destinationBuilding);
+            resultListBA = graph.ShortestPath(startingBuilding, destinationBuilding); // Shortest path is determined between startingBuilding and destinationBuilding
+                                                                                      // It is then stored in the resultListBa variable
 
             List<Tuple<Vector2, Vector2>> resultListAB = new List<Tuple<Vector2, Vector2>>(); // End result that should be returned
 
@@ -28,7 +29,7 @@ namespace EntryPoint
             return resultListAB; // Returning end result
         } 
 
-        private static Graph insertGraph(Graph graph, List<Tuple<Vector2, Vector2>> roadslist, Vector2 startPoint, Vector2 endPoint)
+        private static Graph InsertGraph(Graph graph, List<Tuple<Vector2, Vector2>> roadslist, Vector2 startPoint, Vector2 endPoint)
         {
             graph.AddNode(startPoint); // Adds startingBuilding as FIRST NODE to the graph
 
@@ -53,65 +54,70 @@ namespace EntryPoint
     {
         Dictionary<Vector2, Dictionary<Vector2, int>> vertices = new Dictionary<Vector2, Dictionary<Vector2, int>>();
 
+        public Dictionary<Vector2, Dictionary<Vector2, int>> getVertices() // This function returns the value for the vertices variable, since it is a private variable in the Graph class
+        {
+            return vertices;
+        }
+
         public void AddNode(Vector2 location)
         {
-            if (!vertices.ContainsKey(location)) //!false == true;
+            if (!vertices.ContainsKey(location)) // Checks if the vertices dictionary contains a key with the value given for location. 
+                                                 // If that is not the case (= false), then it converts to true, which means the if block is entered
             {
                 Dictionary<Vector2, int> emptyRoadsList = new Dictionary<Vector2, int>();
-                vertices[location] = emptyRoadsList;
+                vertices[location] = emptyRoadsList; // In the dictionary with the key for location, the value for emptyRoadsList
             }
         }
 
-        public void AddRoad(Vector2 roadpoint1, Vector2 roadpoint2, int length)
+        public void AddRoad(Vector2 roadpoint1, Vector2 roadpoint2, int length_of_road)
         {
-            if (!vertices[roadpoint1].ContainsKey(roadpoint2))
+            if (!vertices[roadpoint1].ContainsKey(roadpoint2)) // Checks if the value in the vertices dictionary with the roadpoint1 key contains a key with the value for roadPoint2
+                                                               // If that is not the case (= false), then it converts to true, which means the if block is entered
             {
-              vertices[roadpoint1].Add(roadpoint2, length);
+               vertices[roadpoint1].Add(roadpoint2, length_of_road); // Adds a new dictionary (= new value for key roadpoint1) with vector2 = roadPoint2 and int = length_of_road to the vertices dictionary
             }
 
-            if (!vertices[roadpoint2].ContainsKey(roadpoint1))
+            if (!vertices[roadpoint2].ContainsKey(roadpoint1)) // Checks if the value in the vertices dictionary with the roadpoint2 key contains a key with the value for roadPoint1
+                                                               // If that is not the case (= false), then it converts to true, which means the if block is entered
             {
-              vertices[roadpoint2].Add(roadpoint1, length);
+               vertices[roadpoint2].Add(roadpoint1, length_of_road); // Adds a new dictionary (= new value for key roadpoint2) with vector1 = roadPoint2 and int = length_of_road to the vertices dictionary
             }
-        }
-
-        public Dictionary<Vector2, Dictionary<Vector2, int>> getVertices()
-        {
-          return vertices;
         }
 
         public List<Tuple<Vector2, Vector2>> ShortestPath(Vector2 startPoint, Vector2 endPoint)
         {
             var previous = new Dictionary<Vector2, Vector2>();
             var distances = new Dictionary<Vector2, int>();
-            var nodes = new List<Vector2>();
+            List<Vector2> nodes = new List<Vector2>();
 
-            List<Tuple<Vector2, Vector2>> path = null;
+            List<Tuple<Vector2, Vector2>> path = null; // Resulting shortest path is empty in the beginning (stating the obvious here). 
+                                                       // Is returned by the ShortestPath function
 
             foreach (var node in vertices)
             {
-                if (node.Key == startPoint)
+                if (node.Key == startPoint) // startPoint = Vector2 for the house.  Checks if it matches with any of the keys in the vertices dictionary
                 {
-                  distances[node.Key] = 0;
+                  distances[node.Key] = 0; // Its key in the distances dictionary will be set to 0
                 }
 
                 else
                 {
-                  distances[node.Key] = int.MaxValue;
+                  distances[node.Key] = int.MaxValue; // Key in distances dictionary if set to the MaxValue that the int variable type 
+                                                      // This is done in order to (somehow) achieve infinity
                 }
 
-                nodes.Add(node.Key);
+                nodes.Add(node.Key); // Adds the current Vector2 of vertices to the nodes list
             }
 
-            while (nodes.Count != 0)
+            while (nodes.Count > 0)
             {
                 nodes.Sort((x, y) => distances[x] - distances[y]);
-                var smallest = nodes[0];
-                nodes.Remove(smallest);
+                var smallest = nodes[0]; // Selects the first element of the nodes list
+                nodes.Remove(smallest); // Removes the smallest node from the nodes list. In other words, the 1st element of the list will always be removed
 
-                if (smallest == endPoint)
+                if (smallest == endPoint)  // Checks f the smallest road is equal to the end road
                 {
-                    path = new List<Tuple<Vector2, Vector2>>();
+                    path = new List<Tuple<Vector2, Vector2>>(); // Path variable is instantiated
 
                     while (previous.ContainsKey(smallest))
                     {
@@ -129,7 +135,7 @@ namespace EntryPoint
                   break;
                 }
 
-                foreach (var neighbor in vertices[smallest])
+                foreach (var neighbor in vertices[smallest]) // Goes through each Dictionary value for the key (with value smallest) in vertices dictionary
                 {
                     var totalDistance = distances[smallest] + neighbor.Value;
 
@@ -142,6 +148,7 @@ namespace EntryPoint
             }
 
             return path;
+            // Dictionary<Vector2, Dictionary<Vector2, int>> vertices = new Dictionary<Vector2, Dictionary<Vector2, int>>();
         }
     }
 }
